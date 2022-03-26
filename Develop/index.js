19fd131bcfedde99b9a309475e1d9e8bbe879a33
JulianF <floydjulian1991@gmail.com>
2022-03-26 14:49:39 -0700

    Added the inquirer questions for two functions,questionsUser and questionsProject. Still need to add some options for license list, need to add promise chain and validate data, need to add interpolated markdown content for the generated readme, need to write function to write to file

--- Develop/index.js
+++ Develop/index.js
@@ -19,10 +19,60 @@ const questionsUser = () => {
             type: 'input',
             name: 'about',
             message: 'Tell us a little about yourself:'
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is your Email address?'
         }
     ]);
 };
 
+const questionsProject = () => {
+    console.log(`
+        ===================
+        Create a New README
+        ===================
+    `);
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the title of your project for the README?'
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Enter the Description of your project for the README:'   
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'Enter the Installation Instructions for your project README:'
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'Enter your Usage section for your project README:'
+        },
+        {
+            type: 'input',
+            name: 'contribution',
+            message: 'Enter your Contributing guidelines section for your project README:'
+        },
+        {
+            type: 'input',
+            name: 'test',
+            message: 'Enter your Test Instructions for your project README:'
+        },
+        {
+            type: 'list',
+            name: 'license',
+            message: 'Please select a license for your project/application:'
+        }
+    ])
+}
+questionsUser().then(answers => console.log(answers));
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
@@ -31,3 +81,4 @@ function init() {}
 
 // Function call to initialize app
 init();
+
