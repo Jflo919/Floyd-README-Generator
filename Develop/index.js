cc54bb718c550d0b499cfc660307ff87af0cb0fc
JulianF <floydjulian1991@gmail.com>
2022-03-26 13:54:55 -0700

    asdas

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,27 @@
 // TODO: Include packages needed for this application
-
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateReadMe = require('./utils/generateMarkdown.js');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questionsUser = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'    
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'Enter your GitHub Username'
+        },
+        {
+            type: 'input',
+            name: 'about',
+            message: 'Tell us a little about yourself:'
+        }
+    ]);
+};
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
