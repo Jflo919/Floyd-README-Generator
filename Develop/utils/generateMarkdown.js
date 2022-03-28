7c7040b217376d1b061b7e1811663820f9b9e59f
JulianF <floydjulian1991@gmail.com>
2022-03-28 13:00:49 -0700

    Added md README code, still needs links, look up how to do it in markdown, still need to chain promises, and functions to renderlicensebadhe thru renderlicensesection

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -13,6 +13,25 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+          ## Description:
+        ${data.description}
+        ### Table of Contents
+
+        ### Installation:
+        ${data.installation}
+        ### Usage Instructions:
+        ${data.usage}
+        ### License:
+        ${data.license}
+        ### Contribution Guidelines:
+        ${data.contribution}
+        ### Test Instructions:
+        ${data.test}
+        ### Questions/About 
+        ${data.name}
+        ${data.github}  Put md link outside of data.github, so users' can click on link to github profile
+        * If you need to reach me for additional questions, please contact me by email:
+        ${data.email}
 
 `;
 }
